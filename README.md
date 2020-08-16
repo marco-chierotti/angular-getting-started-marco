de1bcea39d985a7f1a7c95177c0f182730993cb4
marco-chierotti <marco.chierotti@outlook.com>
2020-08-15 21:28:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-getting-started-marco
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-getting-started-marco)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  // notify handler for the event from the product alert component
+  onNotify(event) {
+    window.alert('You will be notified when the "' + event.name + '" product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
